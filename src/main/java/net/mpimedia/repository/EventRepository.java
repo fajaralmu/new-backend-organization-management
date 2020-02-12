96c8b8a3c64174556331e806f03c442b011b22d4
Fajar AM <somabangsa@gmail.com>
2020-02-12 22:54:55 +0700

    fix query

--- src/main/java/net/mpimedia/repository/EventRepository.java
+++ src/main/java/net/mpimedia/repository/EventRepository.java
@@ -10,9 +10,10 @@ import net.mpimedia.entity.Event;
 public interface EventRepository  extends JpaRepository<Event, Long>, RepositoryCustom<Event>{
 
 	@Query(nativeQuery = true,
-			value = "select * from event left join  section  ON program.section_id =  section.id"
-					+ " left join division on division.id =  section.division_id"
-					+ " where MONTH(event.date)=?1 AND YEAR(event.date) = ?2"
+			value = "select * from event left join program on event.program_id = program.id "
+					+ " left join  section  ON program.section_id =  section.id "
+					+ " left join division on division.id =  section.division_id "
+					+ " where MONTH(event.date)=?1 AND YEAR(event.date) = ?2 "
 					+ " AND division.id=?3"
 			)
 	List<Event> getByMonthAndYear(int month, int year, long divisionId);
