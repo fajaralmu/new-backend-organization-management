21b3e019400b2bf54cabd26c71374371572b7b56
Fajar AM <somabangsa@gmail.com>
2020-02-13 20:10:20 +0700

    comments websocket things

--- src/main/java/net/mpimedia/service/RealtimeService2.java
+++ src/main/java/net/mpimedia/service/RealtimeService2.java
@@ -1,46 +1,46 @@
-package net.mpimedia.service;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.stereotype.Service;
-
-import net.mpimedia.dto.WebResponse;
-
-@Service
-public class RealtimeService2 {
-	Logger log = LoggerFactory.getLogger(RealtimeService2.class);
-
-	@Autowired
-	private SimpMessagingTemplate webSocket; 
-
-	public RealtimeService2() {
-		LogProxyFactory.setLoggers(this);
-		log.info("=======================REALTIME SERVICE 2======================="); 
-	}
-
-
-	public boolean sendUpdateSession(Object payload) {
- 
-		webSocket.convertAndSend("/wsResp/sessions", payload);
-
-		return true;
-	}
-
-	public void sendProgress(double progress, String requestId) {
-		System.out.println(">>>>>>>>>>SEND PROGRESS:" + progress + " (" + requestId + ")");
-		sendProgress(WebResponse.builder().requestId(requestId).percentage(progress).build());
-	}
-
-	public void sendProgress(WebResponse shopApiResponse) {
-		webSocket.convertAndSend("/wsResp/progress", shopApiResponse);
-	}
-
-
-	public void sendMessageChat(WebResponse response) {
-		webSocket.convertAndSend("/wsResp/messages", response);
-		
-	}
-
-}
+//package net.mpimedia.service;
+//
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.messaging.simp.SimpMessagingTemplate;
+//import org.springframework.stereotype.Service;
+//
+//import net.mpimedia.dto.WebResponse;
+//
+//@Service
+//public class RealtimeService2 {
+//	Logger log = LoggerFactory.getLogger(RealtimeService2.class);
+//
+//	@Autowired
+//	private SimpMessagingTemplate webSocket; 
+//
+//	public RealtimeService2() {
+//		LogProxyFactory.setLoggers(this);
+//		log.info("=======================REALTIME SERVICE 2======================="); 
+//	}
+//
+//
+//	public boolean sendUpdateSession(Object payload) {
+// 
+//		webSocket.convertAndSend("/wsResp/sessions", payload);
+//
+//		return true;
+//	}
+//
+//	public void sendProgress(double progress, String requestId) {
+//		System.out.println(">>>>>>>>>>SEND PROGRESS:" + progress + " (" + requestId + ")");
+//		sendProgress(WebResponse.builder().requestId(requestId).percentage(progress).build());
+//	}
+//
+//	public void sendProgress(WebResponse shopApiResponse) {
+//		webSocket.convertAndSend("/wsResp/progress", shopApiResponse);
+//	}
+//
+//
+//	public void sendMessageChat(WebResponse response) {
+//		webSocket.convertAndSend("/wsResp/messages", response);
+//		
+//	}
+//
+//}
